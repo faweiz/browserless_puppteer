6c090edd3630f3292aa83038144cbe80ed17f718
Fawei Zhang <fzhang58@jh.edu>
2022-12-30 16:51:18 -0500

    12302022 1

--- index.js
+++ index.js
@@ -52,4 +52,50 @@ app.get('/title', async (req, res) => {
     console.log(page_title);
     browser.close();
 });
+
+
+
+
+app.get('/properties/v2/detail', async (req, res, next) => {
+  const zpid = req.query.property_id;
+  let properties_detail = [];
+  const zpid_url = `https://www.zillow.com/homedetails/${zpid}_zpid/`;
+
+
+  (async() =>{
+  // Puppeteer
+      const browser = await puppeteer.connect({
+        browserWSEndpoint: 'wss://chrome.browserless.io?token=da549f5d-deea-4389-9fca-f088af72b3a1' 
+      });
+
+      const page = await browser.newPage();
+      await page.goto(zpid_url);
+
+      let quotes = await page.evaluate(() => {
+          let quotes = document.body.querySelector('script[id="hdpApolloPreloadedData"]').textContent;
+          return quotes;
+      });
+
+      let quotes_all = JSON.parse(quotes);
+      let apiCache = JSON.parse(quotes_all.apiCache);
+      let property_data = apiCache[`ForSaleShopperPlatformFullRenderQuery{\"zpid\":${zpid},\"contactFormRenderParameter\":{\"zpid\":${zpid},\"platform\":\"desktop\",\"isDoubleScroll\":true}}`].property;
+      console.log(property_data);
+
+      properties_detail.push(property_data);
+      res.json(properties_detail);
+
+      await page.close();
+      await browser.close();
+  })();
+
+});
+
+
+
+
+
+
+
+
+
 app.listen(8080, () => console.log('Listening on PORT: 8080'));
